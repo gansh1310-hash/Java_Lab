a39e27747e8571a746d3d916ec0d7ecda37fb37d
gansh1310-hash <gansh1310@gmail.com>
2026-05-01 12:00:38 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,44 +1,56 @@
-class Shape {
-    void area() {
-        System.out.println("Area of shape");
+
+interface Gross {
+    double calculateGross();
+}
+
+
+class Employee {
+    int empId;
+    String name;
+    double basic;
+
+    // Constructor
+    Employee(int empId, String name, double basic) {
+        this.empId = empId;
+        this.name = name;
+        this.basic = basic;
+    }
+
+    void displayEmployee() {
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Name: " + name);
+        System.out.println("Basic Salary: " + basic);
     }
 }
 
-class Square extends Shape {
-    int side = 4;
 
-    void area() {
-        System.out.println("Area of Square = " + (side * side));
+class Salary extends Employee implements Gross {
+    double hra, da;
+
+    Salary(int empId, String name, double basic, double hra, double da) {
+        super(empId, name, basic);
+        this.hra = hra;
+        this.da = da;
+    }
+
+    
+    public double calculateGross() {
+        return basic + hra + da;
+    }
+
+    void displaySalary() {
+        displayEmployee();
+        System.out.println("HRA: " + hra);
+        System.out.println("DA: " + da);
+        System.out.println("Gross Salary: " + calculateGross());
     }
 }
 
-class Rectangle extends Shape {
-    int length = 5;
-    int breadth = 3;
 
-    void area() {
-        System.out.println("Area of Rectangle = " + (length * breadth));
-    }
-}
-
-class Circle extends Shape {
-    int radius = 3;
-
-    void area() {
-        System.out.println("Area of Circle = " + (3.14 * radius * radius));
-    }
-}
-
-public class Main {
+public class Main
+ {
     public static void main(String[] args) {
-
-        Square s = new Square();
-        s.area();
-
-        Rectangle r = new Rectangle();
-        r.area();
-
-        Circle c = new Circle();
-        c.area();
+        Salary s = new Salary(101, "Ansh", 20000, 5000, 3000);
+        s.displaySalary();
     }
 }
